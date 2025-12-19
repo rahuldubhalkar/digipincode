0832916b6ee2fbac015dc78ce7acc958a375f35e
rahul dubhalkar <rdubhalkar10@gmail.com>
2025-12-19 15:06:04 +0000

    remoove the api which i used for digipin and unwanted api wihich i have

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,11 +3,17 @@
 import type { PostOffice } from './types';
 import { unstable_noStore as noStore } from 'next/cache';
 
-const API_KEY = process.env.API_KEY || '579b464db66ec23bdd000001f96a317e4daa449850b07ef3ce5c9f4a';
+const API_KEY = process.env.API_KEY;
 const API_URL = 'https://api.data.gov.in/resource/6176ee09-3d56-4a3b-8115-21841576b2f6';
 
 async function fetchFromAPI(filters: Record<string, string>, limit: number = 1000, offset: number = 0): Promise<any[]> {
   noStore();
+  
+  if (!API_KEY) {
+    console.error('API_KEY is not set. Please provide it as an environment variable.');
+    return [];
+  }
+
   const params = new URLSearchParams({
     'api-key': API_KEY,
     'format': 'json',
